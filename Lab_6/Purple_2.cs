2b0f70485984e9043ae85a568b9faae6bb978acf
Владислав Куличков <vlad.kulichkov.06@mail.ru>
2025-03-06 00:12:07 +0300

    Add files via upload

---
+++ Lab_6/Purple_2.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace Lab_6
+{
+    public class Purple_2
+    {
+        public class Participant
+        {
+            private string _name;
+            private string _surname;
+            private double _distance;
+            private int[] _marks;
+
+            public string Name => _name;
+            public string Surname => _surname;
+            public double Distance => _distance;
+            public int[] Marks
+            {
+                get
+                {
+                    if (_marks == null) return null;
+
+                    int[] makrsCopy = new int[_marks.Length];
+                    Array.Copy(_marks, makrsCopy, _marks.Length);
+                    return makrsCopy;
+                }
+            }
+            public double Result { get; private set; }
+
+            public Participant(string name, string surname)
+            {
+                _name = name;
+                _surname = surname;
+                _marks = new int[5];
+            }
+            
+            public void Jump(int distance, int[] marks)
+            {
+                if (marks == null || _marks == null) return;
+                _distance = distance;
+                Array.Copy(marks, _marks, _marks.Length);
+
+                int distancePoints = 60 + (distance - 120) * 2;
+                distancePoints = distancePoints < 0 ? 0 : distancePoints;
+                Result = distancePoints + marks.Sum() - marks.Min() - marks.Max();
+            }
+
+            public static void Sort(Participant[] array)
+            {
+                if (array == null) return;
+
+                var sortedArray = array.OrderByDescending(x => x.Result).ToArray();
+                Array.Copy(sortedArray, array, array.Length);
+            }
+
+            public static void PrintHead()
+            {
+                Console.WriteLine(_printItem("Name") + _printItem("Surname") + _printItem("Result"));
+            }
+            public void Print()
+            {
+                Console.WriteLine($"{_printItem(_name)} {_printItem(_surname)} {_printItem(Result.ToString())}");
+            }
+            private static string _printItem(string item)
+            {
+                return item + new string(' ', 15 - item.Length);
+            }
+        }
+    }
+}
---
+++ Lab_6/Purple_3.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    public class Purple_3
+    {
+        public struct Participant
+        {
+            private string _name;
+            private string _surname;
+            private double[] _marks;
+            private int[] _places;
+            private int _score;
+            private int _currentJudge;
+
+            public string Name => _name;
+            public string Surname => _surname;
+            public int Score => _score;
+            public double[] Marks
+            {
+                get 
+                {
+                    if (_marks == null) return null;
+
+                    double[] marksCopy = new double[_marks.Length];
+                    Array.Copy(_marks, marksCopy, _marks.Length);
+                    return marksCopy;
+                }
+            }
+            public int[] Places
+            {
+                get
+                {
+                    if (_places == null) return null;
+
+                    int[] placesCopy = new int[_places.Length];
+                    Array.Copy(_places, placesCopy, _places.Length);
+                    return placesCopy;
+                }
+            }
+
+            public Participant(string name, string surname)
+            {
+                _name = name;
+                _surname = surname;
+                _marks = new double[7];
+                _places = new int[7];
+                _score = 0;
+                _currentJudge = 0;
+            }
+            public void Evaluate(double result)
+            {
+                if (_currentJudge >= 7 || _marks == null) return;
+
+                _marks[_currentJudge] = result;
+                _currentJudge++;
+            }
+            public static void SetPlaces(Participant[] participants)
+            {
+                if (participants == null) return;
+
+                for (int i = 0; i < 7; i++)
+                {
+                    var sortedParticipants = participants.OrderByDescending(x => x.Marks[i]).ToArray();
+
+                    for (int j = 0; j < participants.Length; j++)
+                    {
+                        sortedParticipants[j]._places[i] = j + 1;
+                        sortedParticipants[j]._score += (j + 1);
+                    }
+
+                    Array.Copy(sortedParticipants, participants, participants.Length);
+                }
+            }
+            public static void Sort(Participant[] array)
+            {
+                if (array == null) return;
+
+                var sortedArray = array.OrderBy(x => x.Score).ThenBy(x => x.Places.Min()).ThenByDescending(x => x.Marks.Sum()).ToArray();
+                Array.Copy(sortedArray, array, array.Length);
+            }
+            public static void PrintHead()
+            {
+                Console.WriteLine(_printItem("Name") + _printItem("Surname") + _printItem("Score") + _printItem("TopPlace") + _printItem("TotalMark"));
+            }
+            public void Print()
+            {
+                Console.WriteLine($"{_printItem(Name)} {_printItem(Surname)} {_printItem(Score.ToString())} {_printItem(Places.Min().ToString())} {_printItem(Math.Round(Marks.Sum(), 2).ToString())}");
+            }
+            private static string _printItem(string item)
+            {
+                return item + new string(' ', 15 - item.Length);
+            }
+        }
+    }
+}
